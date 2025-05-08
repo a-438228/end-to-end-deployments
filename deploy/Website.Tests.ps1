e1ec455ea95b09e48c76742fb29a2d669cfc652a
Alex Bannister <alex.bannister@naveocommerce.com>
2025-05-08 17:30:44 +0100

    Update test descriptions and adjust health check endpoint in Website.Tests.ps1

--- deploy/Website.Tests.ps1
+++ deploy/Website.Tests.ps1
@@ -4,7 +4,7 @@ param(
   [string] $HostName
 )
 
-Describe 'Toy Website' {
+Describe 'Website' {
 
     It 'Serves pages over HTTPS' {
       $request = [System.Net.WebRequest]::Create("https://$HostName/")
@@ -20,8 +20,8 @@ Describe 'Toy Website' {
         Should -BeGreaterOrEqual 300 -Because "HTTP is not secure"
     }
 
-    It 'Returns a success code from the health check endpoint' {
-      $response = Invoke-WebRequest -Uri "https://$HostName/health" -SkipHttpErrorCheck
+    It 'Returns a success code from the landing page' {
+      $response = Invoke-WebRequest -Uri "https://$HostName/" -SkipHttpErrorCheck
       Write-Host $response.Content
       $response.StatusCode |
         Should -Be 200 -Because "the website and configuration should be healthy"
